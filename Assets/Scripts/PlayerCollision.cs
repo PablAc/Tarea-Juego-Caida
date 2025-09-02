0d628a6d7a1adf9f6c34e3f07d3df13108d31ee2
PablAc <142343061+PablAc@users.noreply.github.com>
2025-09-02 17:33:18 -0300

    Finished

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class PlayerCollision : MonoBehaviour
 {
     CharacterController myController;
+    [SerializeField] GameObject camera;
 
     private void Awake()
     {
@@ -19,6 +20,11 @@ public class PlayerCollision : MonoBehaviour
         {
             float x = collision.GetComponent<SpdChanger>().alterSpeed;
             myController.changeMaxFallSpeed(x);
+        }
+        else if (collision.CompareTag("Cam"))
+        {
+            CameraController cc = camera.GetComponent<CameraController>();
+            cc.enabled = false;
         }
     }
 }
